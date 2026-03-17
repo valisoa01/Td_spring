a44e5b516cb45eeec62debb6485a573ad6057d3b
valisoa01 <valisoatolotriniaina@gmail.com>
2026-03-17 16:32:33 +0300

    first commit in spring-boot

---
+++ src/main/java/com/tdspring/TdSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tdspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdspring/TdSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
