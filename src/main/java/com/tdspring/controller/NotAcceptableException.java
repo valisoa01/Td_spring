18517ee23daac4a453d010a6bfe8a124b50b622e
valisoa01 <valisoatolotriniaina@gmail.com>
2026-03-25 22:13:55 +0300

    feat:add NotAcceptableException

---
+++ src/main/java/com/tdspring/controller/NotAcceptableException.java
@@ -0,0 +1,7 @@
+package com.tdspring.controller;
+
+public class NotAcceptableException extends RuntimeException {
+    public NotAcceptableException(String message) {
+        super(message);
+    }
+}
